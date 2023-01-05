f38d41b05dc530fad806d5af40e0aee485c30524
MrWillian <willian.dev10@gmail.com>
2023-01-04 21:11:03 -0300

    refactor(recipe): implement recipe props

--- app/recipes/Recipe.tsx
+++ app/recipes/Recipe.tsx
@@ -1,14 +1,18 @@
-export default function Recipe() {
+export default function Recipe({ recipe }: any) {
     return (
-        <div className="flex flex-col justify-around items-center bg-slate-200 rounded-lg shadow-xl m-5 p-2">
-            <img 
-                className="w-96 h-96 rounded-t-xl" 
-                src="https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" 
-                alt="Recipe"
-            />
-            <p className="text-lg font-bold text-slate-600">Teste</p>
+        <div 
+            className="flex flex-col justify-around items-center bg-slate-200 rounded-lg shadow-xl m-5 p-2" 
+            key={recipe.label}
+        >
+            <a href={recipe.url} target="_blank">
+                <img className="w-96 h-96 rounded-t-xl" src={recipe.image} alt={recipe.label} />
+            </a>
+
+            <p className="text-lg font-bold text-slate-600">{recipe.label}</p>
             <ol className="list-none">
-                <li className="text-sm font-bold text-slate-600">Ingrediente de Teste</li>
+                {recipe.ingredientLines.map((ingredient: any) => (
+                    <li className="text-sm font-bold text-slate-600">{ingredient}</li>
+                ))}
             </ol>
         </div>
     )
